6622999c6f743984137f6d4249753fa2204ed6d2
Martin <martin.knowles@hotmail.se>
2020-12-07 15:23:26 +0100

    database classes added

--- DatabaseConnection/Model.cs
+++ DatabaseConnection/Model.cs
@@ -4,19 +4,26 @@ using System.Text;
 
 namespace DatabaseConnection
 {
-    public class Customer
+   public class Customer
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public virtual List<Rental> Sales { get; set; }
+        public string Användarnamn { get; set; }
+        public string Lösenord { get; set; }
+        public string Telefonnummer { get; set; }
+        public string mailaddress { get; set; }
+        public string postaddress { get; set; }
+        public virtual List<Rental> Rentals { get; set; }
     }
+    
     public class Movie
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string ImageURL { get; set; }
-        public virtual List<Rental> Sales { get; set; }
+        public string Filmtitel { get; set; }
+        public string Bild { get; set; }
+        public virtual List<Rental> Rentals { get; set; }
     }
+    
+    
     public class Rental
     {
         public int Id { get; set; }
